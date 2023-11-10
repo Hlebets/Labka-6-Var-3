98f2491e8e3988905b1cb38158f056b00a2576b2
Hlebéts <glib.kobryn@gmail.com>
2023-11-11 01:16:02 +0200

    lvlup of BattleEvent class and save/load game func

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 class Hero:
@@ -17,6 +18,18 @@ class Hero:
     def show_stats(self):
         print(f"Hero has {self.health} health and {self.damage} damage.")
 
+class Enemy:
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            print("Enemy is defeated")
+        else:
+            print(f"Enemy takes {damage} damage and has {self.health} HP left.")
 
 class NothingEvent:
     def __init__(self):
@@ -49,12 +62,41 @@ class BattleEvent:
 
     def interact(self):
         print(self.description)
+        enemy = Enemy(random.randint(5, 10), random.randint(2, 5))
+        hero = Hero(20, 7)
+        while True:
+            print("1. Fight")
+            print("2. Run")
+            print("3. Open Stats")
+            action_input = input("Enter your choice: ")
+
+            if action_input == "1":
+                hero.take_damage(random.randint(0, enemy.damage))
+                enemy.take_damage(random.randint(0, hero.damage))
+                if enemy.health <= 0:
+                    print("Enemy defeated!")
+                    break
+            elif action_input == "2":
+                print("You ran away.")
+                break
+            elif action_input == "3":
+                print(
+                    f"Hero stats: {hero.show_stats()}"
+                )
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
 
 
-class Enemy:
-    def __init__(self, health, damage):
-        self.health = health
-        self.damage = damage
 
-    def take_damage(self, hero):
-        hero.take_damage(self.damage)
+def save_game(hero, completed_events):
+    game_state = {"hero": hero.__dict__, "completed_events": completed_events}
+    with open("savegame.json", "w") as save_file:
+        json.dump(game_state, save_file)
+
+
+def load_game():
+    with open("savegame.json", "r") as save_file:
+        game_state = json.load(save_file)
+        hero = Hero(**game_state["hero"])
+        completed_events = game_state["completed_events"]
+        return hero, completed_events
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import random
-from classes import NothingEvent, FortuneTellerEvent, BattleEvent, Hero, Enemy
+from tkinter import N
+from classes import (
+    NothingEvent,
+    FortuneTellerEvent,
+    BattleEvent,
+    Hero,
+    Enemy,
+    save_game,
+    load_game,
+)
 import random
+import json
 
+hero = Hero(20, 7)
+event = None
 completed_events = 0
-hero = Hero(random.randint(10, 20), random.randint(3, 5))
 
 while True:
     print("Small World - A small text RPG game")
@@ -11,55 +22,82 @@ while True:
     print("2. Continue")
     print("q. Exit")
     main_menu_input = input("Enter your choice: ")
+
     if main_menu_input == "1":
         print("New Game starts now!")
+
         while True:
             print("1. Go Somewhere")
             print("2. Open Stats")
+            print("3. Save game and return to main menu")
             action_input = input("Enter your choice: ")
+
             if action_input == "1":
                 print("You are going in a chosen direction")
                 event_type = random.choice(["Nothing", "FortuneTeller", "Battle"])
-                event = None
-                enemy = None
                 if event_type == "Nothing":
                     event = NothingEvent()
                 elif event_type == "FortuneTeller":
                     event = FortuneTellerEvent()
                 elif event_type == "Battle":
                     event = BattleEvent()
-                    enemy = Enemy(random.randint(10, 20), random.randint(3, 5))
 
                 if event is not None:
                     event.interact()
-                else:
-                    ("Unexpected event type")
-                completed_events += 1
-                print("1. Fight")
-                print("2. Run")
-                print("3. Open Stats")
-                input("Enter your choice: ")
-                if action_input == "1":
-                    print("You are fighting")
-                elif action_input == "2":
-                    print("You are running")
-                    if enemy is not None:
-                        print("Enemy is attacking")
-                        print(
-                            f"Hero received {enemy.damage} damage. {hero.health} HP left."
-                        )
-                elif action_input == "3":
-                    hero.show_stats()
-                else:
-                    print("Invalid input, try again")
+                    completed_events += 1
+
             elif action_input == "2":
                 print("\nYou have opened stats")
                 hero.show_stats()
                 print(f"You have completed {completed_events} events")
+
+            elif action_input == "3":
+                print("Saving game...")
+                save_game(hero, completed_events)
+                break
+
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
     elif main_menu_input == "2":
         print("Continue")
+        load_game()
+
+        while True:
+            print("1. Go Somewhere")
+            print("2. Open Stats")
+            print("3. Save game and return to main menu")
+            action_input = input("Enter your choice: ")
+
+            if action_input == "1":
+                print("You are going in a chosen direction")
+                event_type = random.choice(["Nothing", "FortuneTeller", "Battle"])
+                if event_type == "Nothing":
+                    event = NothingEvent()
+                elif event_type == "FortuneTeller":
+                    event = FortuneTellerEvent()
+                elif event_type == "Battle":
+                    event = BattleEvent()
+
+                if event is not None:
+                    event.interact()
+                    completed_events += 1
+
+            elif action_input == "2":
+                print("\nYou have opened stats")
+                hero.show_stats()
+                print(f"You have completed {completed_events} events")
+
+            elif action_input == "3":
+                print("Saving game...")
+                save_game(hero, completed_events)
+                break
+
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
+
     elif main_menu_input == "q":
         print("Exit")
         break
+
     else:
         print("Invalid input")
